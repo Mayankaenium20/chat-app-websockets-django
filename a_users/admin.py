12bb1e61ad20c62e3d6be2731106b5ffe763d4a4
Mayankaenium20 <mayank.baber20@gmail.com>
2025-07-15 14:51:37 +0530

    new fresh start

--- a_users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from a_rtchat.models import ChatGroup, GroupMessage
-from .models import Profile
-
-admin.site.register(Profile)
-admin.site.register(ChatGroup)
-admin.site.register(GroupMessage)
-
